4e8b3c545fb34b28d2294b5f2535e093ffd6be26
Amal4m41 <amalsunil117@gmail.com>
2021-11-12 14:06:29 +0400

    Modified the widgets to be more efficient in rebuilds

--- provider_in_depth_wax_app/lib/screens/home_screen.dart
+++ provider_in_depth_wax_app/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+import 'package:provider_in_depth_wax_app/providers/settings_provider.dart';
 import 'package:provider_in_depth_wax_app/screens/settings_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,6 +9,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print("HOMESCREEN BUILD");
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -23,7 +26,8 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Container(
         child: Center(
-          child: Text("HOME SCREEN"),
+          //will only update/rebuild (the nearest build method) this widget if this specific value changes.
+          child: Text(context.select((SettingsProvider value) => value.units)),
         ),
       ),
     );
--- provider_in_depth_wax_app/lib/screens/settings_screen.dart
+++ provider_in_depth_wax_app/lib/screens/settings_screen.dart
@@ -19,57 +19,74 @@ class SettingsScreen extends StatelessWidget {
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Units"),
-                DropdownButton<String>(
-                  value: context.watch<SettingsProvider>().units,
-                  onChanged: (String? value) {
-                    if (value != null) {
-                      context.read<SettingsProvider>().setUnits(value);
-                    }
-                  },
-                  items: ["Metric", "Imperial"]
-                      .map<DropdownMenuItem<String>>((String item) =>
-                          DropdownMenuItem(child: Text(item), value: item))
-                      .toList(),
-                )
-              ],
+              children: [Text("Units"), DropDownWidget()],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
+              children: const [
                 Text("Wax Lines"),
-                Wrap(
-                  spacing: 5,
-                  runSpacing: 4,
-                  children: [
-                    FilterChip(
-                        label: Text("Swix"),
-                        selected: context
-                            .watch<SettingsProvider>()
-                            .waxLines
-                            .contains("Swix"),
-                        selectedColor: Colors.lightBlueAccent.shade100,
-                        onSelected: (bool value) {
-                          context.read<SettingsProvider>().editWaxLines("Swix");
-                        }),
-                    FilterChip(
-                        label: Text("Toko"),
-                        selected: context
-                            .watch<SettingsProvider>()
-                            .waxLines
-                            .contains("Toko"),
-                        selectedColor: Colors.lightBlueAccent.shade100,
-                        onSelected: (bool value) {
-                          context.read<SettingsProvider>().editWaxLines("Toko");
-                        }),
-                  ],
-                ),
+                FiltChipsWidget(),
               ],
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class FiltChipsWidget extends StatelessWidget {
+  const FiltChipsWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("FILTERCHIPS  BUILD");
+    return Wrap(
+      spacing: 5,
+      runSpacing: 4,
+      children: [
+        FilterChip(
+            label: Text("Swix"),
+            selected:
+                context.watch<SettingsProvider>().waxLines.contains("Swix"),
+            selectedColor: Colors.lightBlueAccent.shade100,
+            onSelected: (bool value) {
+              context.read<SettingsProvider>().editWaxLines("Swix");
+            }),
+        FilterChip(
+            label: Text("Toko"),
+            selected:
+                context.watch<SettingsProvider>().waxLines.contains("Toko"),
+            selectedColor: Colors.lightBlueAccent.shade100,
+            onSelected: (bool value) {
+              context.read<SettingsProvider>().editWaxLines("Toko");
+            }),
+      ],
+    );
+  }
+}
+
+class DropDownWidget extends StatelessWidget {
+  const DropDownWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("DROPDOWN  BUILD");
+    return DropdownButton<String>(
+      value: context.watch<SettingsProvider>().units,
+      onChanged: (String? value) {
+        if (value != null) {
+          context.read<SettingsProvider>().setUnits(value);
+        }
+      },
+      items: ["Metric", "Imperial"]
+          .map<DropdownMenuItem<String>>(
+              (String item) => DropdownMenuItem(child: Text(item), value: item))
+          .toList(),
     );
   }
 }
